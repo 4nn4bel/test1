8026cbd1b009c975399d4ec9735ee8d4dee8f490
4nn4bel <annabeltengingsing@gmail.com>
2015-02-22 12:50:47 +0800

    first commit by 4nn4bel

---
+++ test1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test1
+//
+//  Created by 4nn4bel on 22/2/15.
+//  Copyright (c) 2015 4nn4bel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
